a6229d69dc62cb87bcf100501bb896fabab901fb
AbbeyRDuBois <abbeydubois8@gmail.com>
2024-03-28 00:14:27 -0700

    Added Dynamic add mew card to GUI

--- card_selector.py
+++ card_selector.py
@@ -1,3 +1,4 @@
+import sys
 import PyQt6.QtWidgets as qtw
 from functools import partial
 
@@ -8,24 +9,25 @@ hand = []
 
 def home_menu(window, func):
     window.rows = 5
-    window.columns = 3
+#    window.rows = 5
+#    window.columns = 3
     
-    centralWidget = qtw.QWidget()
-    window.setCentralWidget(centralWidget)
+#    centralWidget = qtw.QWidget()
+#    window.setCentralWidget(centralWidget)
 
-    window.layout = qtw.QGridLayout(centralWidget)
+#    window.layout = qtw.QGridLayout(centralWidget)
     
-    card_list = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', "Done"]
-    list_length = len(card_list)
+#    card_list = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', "Done"]
+#    list_length = len(card_list)
 
-    i = 0
-    for row in range(window.rows):
-        for column in range(window.columns): 
-            button = qtw.QPushButton(f'{card_list[i]}', window)
-            button.clicked.connect(partial(func, card_list[i]))
-            window.layout.addWidget(button, row+1, column)
-            i += 1
-            if i == list_length: break
+#    i = 0
+#    for row in range(window.rows):
+#        for column in range(window.columns): 
+#            button = qtw.QPushButton(f'{card_list[i]}', window)
+#            button.clicked.connect(partial(func, card_list[i]))
+#            window.layout.addWidget(button, row+1, column)
+#            i += 1
+#            if i == list_length: break
 
 class PushButton(qtw.QPushButton):
     def __init__(self, text, parent=None):
@@ -35,21 +37,89 @@ class PushButton(qtw.QPushButton):
         self.setMinimumSize(qtw.QSize(50, 50))
         self.setMaximumSize(qtw.QSize(50, 50))
 
-class MyWindow(qtw.QMainWindow):
+class Main_Window(qtw.QWidget):
+    cards = 0
+
+    #Initializes the class as a QMainWindow and calls initUI
     def __init__(self):
-        super(MyWindow, self).__init__()
+        super().__init__()
+        self.initUI()
 
-        home_menu(self, self.onClicked)
+    #Creates new rank and suit boxes adds it to layout then increments how many cards you have
+    def add_new_card(self):
+        new_suit = SuitBox()
+        new_rank = RankBox()
+        #Need the + 1 Offset for the Add Card Button
+        self.main_layout.addWidget(new_suit, self.cards + 1, 0)
+        self.main_layout.addWidget(new_rank, self.cards + 1, 1)
+        self.cards += 1
 
-    def onClicked(self, card):
-        global hand
-        print(card)
+    #Creates the Main Window
+    def initUI(self):
+        #self.setWindowTitle('Cribbage Counter')
+        #Set Main window as a Grid Layout
+        self.main_layout = qtw.QGridLayout()
+        self.setLayout(self.main_layout)
 
-        if(card == "Done"):
+        #Add the Add Card button to top of window
+        add_card = qtw.QPushButton("Add Card")
+        add_card.clicked.connect(self.add_new_card)
+        self.main_layout.addWidget(add_card, 0, 0, 1, 2)
+
+        #Have 4 cards already in window
+        i = 0
+        while (i < 4):
+            self.add_new_card()
+            i += 1
+
+#Defines the Combo box for selecting Suit
+class SuitBox(qtw.QComboBox):
+    def __init__(self):
+        super().__init__()
+        self.initBox()
+
+    def initBox(self):
+        self.addItem("Heart")
+        self.addItem("Diamond")
+        self.addItem("Spade")
+        self.addItem("Club")
+
+#Defines the Combo box for selecting Card Rank
+class RankBox(qtw.QComboBox):
+    def __init__(self):
+        super().__init__()
+        self.initBox()
+
+    def initBox(self):
+        self.addItem("Ace")
+        self.addItem("1")
+        self.addItem("2")
+        self.addItem("3")
+        self.addItem("4")
+        self.addItem("5")
+        self.addItem("6")
+        self.addItem("7")
+        self.addItem("8")
+        self.addItem("9")
+        self.addItem("10")
+        self.addItem("Jack")
+        self.addItem("Queen")
+        self.addItem("King")
+
+#    def __init__(self):
+#        super(MyWindow, self).__init__()
+
+#        home_menu(self, self.onClicked)
+
+#    def onClicked(self, card):
+#        global hand
+#        print(card)
+
+#        if(card == "Done"):
             #Navigate to get flipped card
-            home_menu(self, self.onFlippedClick)
-        else:
-            hand.append(crib_calc.Card(card, "heart"))
+#            home_menu(self, self.onFlippedClick)
+#        else:
+#            hand.append(crib_calc.Card(card, "heart"))
 
     def onFlippedClick(self, card):
         global flipped
@@ -72,12 +142,10 @@ class MyWindow(qtw.QMainWindow):
         hand = []
 
         home_menu(self, self.onClicked)
-                
+
 if __name__ == '__main__':
-    app = qtw.QApplication([])
-    window = MyWindow()
-
-    window.setWindowTitle('Cribbage Counter')
+    app = qtw.QApplication(sys.argv)
+    window = Main_Window()
+    window.setGeometry(100,100,400,300)
     window.show()
-
-    app.exec()
+    sys.exit(app.exec())
