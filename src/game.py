1da9aa51d873d4f8eb4d498ee88c6dd13fd19270
Andrew Rice <aprspie@gmail.com>
2024-08-04 17:11:48 -0700

    Typo that fixes nothing :(

--- src/game.py
+++ src/game.py
@@ -340,7 +340,7 @@ async def get_all_hand_pics():
 
     #Create empty image with place for images
     if max_hand_len == 0:
-        max_hand_len == 1
+        max_hand_len = 1
     all_hands_img = Image.new('RGB', (card_width*max_hand_len*sprite_scalar, card_height*sprite_scalar*len(hands)), color=(0, 80, 80))
 
     #Create list of hand pics to delete
